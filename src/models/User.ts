5632705bddbb29f9f09144e325d076d627404416
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-23 22:35:54 +0200

    Extract attributes from User class.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
 	id?: number;
@@ -12,17 +13,11 @@ const rootUrl = 'http://localhost:3000/users/';
 export class User {
 	public events: Eventing;
 	public sync: Sync<UserProps>;
+	public attributes: Attributes<UserProps>;
 
-	constructor(private data: UserProps) {
+	constructor(attrs: UserProps) {
+		this.attributes = new Attributes<UserProps>(attrs);
 		this.events = new Eventing();
-		this.sync = new Sync<UserProps>(rootUrl);
-	}
-
-	get(propName: string): number | string {
-		return this.data[propName];
-	}
-
-	set(props: UserProps): void {
-		Object.assign(this.data, props);
+		this.sync == new Sync<UserProps>(rootUrl);
 	}
 }
