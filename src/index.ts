02b1de98901764868ad1d077ef81cf7b15885196
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-23 09:23:22 +0200

    create user class and basic userProps

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('dklfjsk');
+import { User } from './models/User';
+
+const user = new User({ name: 'piotr', age: 32 });
+console.log(`
+name: ${user.get('name')};
+age: ${user.get('age')}
+`);
