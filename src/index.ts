b7fb8b15d9b7690217537b27ee984fc254fa7256
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-23 14:49:24 +0200

    Start extracting save and fetcg logic to sync class Please enter the commit message for your changes. Lines starting

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import axios from 'axios';
 import { User } from './models/User';
 
 // axios.post('http://localhost:3000/users', { name: 'piotr', age: '32' });
-// const user = new User({ name: 'sjdhfjsh', age: 12 });\
-const user = new User({ id: 15 });
+const user = new User({ name: 'sjdhfjsh', age: 12 });
+user.events.on('change', () => {
+	console.log('change');
+});
+
+user.events.trigger('change');
+// const user = new User({ id: 15 });
 // user.fetch();
 // setTimeout(() => {
-user.set({ name: 'ppo' });
-console.log(user.get('name'));
-user.save();
-console.log(user);
+// user.set({ name: 'ppo' });
+// console.log(user.get('name'));
+// user.save();
+// console.log(user);
 // }, 2000);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,17 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
-interface UserProps {
+export interface UserProps {
 	id?: number;
 	name?: string;
 	age?: number;
 }
 
 export class User {
-	constructor(private data: UserProps) {}
+	public events: Eventing;
+
+	constructor(private data: UserProps) {
+		this.events = new Eventing();
+	}
 
 	get(propName: string): number | string {
 		return this.data[propName];
@@ -15,27 +19,5 @@ export class User {
 
 	set(props: UserProps): void {
 		Object.assign(this.data, props);
-	}
-
-	fetch(): void {
-		axios.get(`http://localhost:3000/users/${this.get('id')}`).then(
-			(response: AxiosResponse): void => {
-				this.set(response.data);
-				console.log(this);
-			}
-		);
-	}
-
-	save(): void {
-		const id = this.get('id');
-		if (id) {
-			axios.put(`http://localhost:3000/users/${id}`, this.data);
-		} else {
-			axios.post('http://localhost:3000/users', this.data).then(
-				(response: AxiosResponse): void => {
-					this.set(response.data);
-				}
-			);
-		}
 	}
 }
