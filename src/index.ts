9ca1c00d657bb6f7bb59003599af581e03bed3df
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-23 09:32:48 +0200

    Add seter for props

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'piotr', age: 32 });
+
+user.set({ age: 38 });
+
 console.log(`
 name: ${user.get('name')};
 age: ${user.get('age')}
