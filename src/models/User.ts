a963caf32adecc7165bec783bde1b69ad4e74370
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-23 14:11:18 +0200

    Extract events logics from User

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,11 +6,7 @@ interface UserProps {
 	age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-	private events: { [key: string]: Callback[] } = {};
-
 	constructor(private data: UserProps) {}
 
 	get(propName: string): number | string {
@@ -19,21 +15,6 @@ export class User {
 
 	set(props: UserProps): void {
 		Object.assign(this.data, props);
-	}
-
-	on(eventName: string, callback: Callback): void {
-		const handlers = this.events[eventName] || [];
-		handlers.push(callback);
-		this.events[eventName] = handlers;
-	}
-
-	trigger(eventName: string): void {
-		const handlers = this.events[eventName];
-
-		if (!handlers || handlers.length === 0) {
-			return;
-		}
-		handlers.forEach(callback => callback());
 	}
 
 	fetch(): void {
