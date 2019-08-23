563da52abcd6e5c87ecd4e718bf0db140d4027ae
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-23 10:18:45 +0200

    Add trriger events method

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import { User } from './models/User';
 const user = new User({ name: 'piotr', age: 32 });
 
 user.on('change', () => {
-	console.log('callback');
+	console.log('callback1');
+});
+user.on('change', () => {
+	console.log('callback2');
 });
 
 console.log(user);
+user.trigger('change');
