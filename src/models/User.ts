b20e380c70b982f2c1b9367c550df5209e33d269
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-23 21:58:15 +0200

    Make Sync generic class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
 export interface UserProps {
 	id?: number;
@@ -6,11 +7,15 @@ export interface UserProps {
 	age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users/';
+
 export class User {
 	public events: Eventing;
+	public sync: Sync<UserProps>;
 
 	constructor(private data: UserProps) {
 		this.events = new Eventing();
+		this.sync = new Sync<UserProps>(rootUrl);
 	}
 
 	get(propName: string): number | string {
