598d69380ac91e6971ddd89ad8eb4021fc6c3ab1
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-26 11:04:48 +0200

    add event liseners to the tamplet

--- src/View/UserForm.ts
+++ src/View/UserForm.ts
@@ -1,18 +1,41 @@
 export class UserForm {
 	constructor(private parent: Element) {}
 
+	eventsMap(): { [key: string]: () => void } {
+		return {
+			'click:button': this.onButtonClick
+		};
+	}
+
+	onButtonClick() {
+		console.log('Hi there.');
+	}
+
 	template(): string {
 		return `
             <div>
                 <h1>User Form</h1>
                 <input />
+                <button>Save</button>
             </div>
         `;
+	}
+
+	bindEvents(fragment: DocumentFragment): void {
+		const eventsMap = this.eventsMap();
+
+		Object.keys(eventsMap).forEach(key => {
+			const [eventName, selector] = key.split(':');
+			fragment.querySelectorAll(selector).forEach(element => {
+				element.addEventListener(eventName, eventsMap[key]);
+			});
+		});
 	}
 
 	render(): void {
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
+		this.bindEvents(templateElement.content);
 		this.parent.append(templateElement.content);
 	}
 }
