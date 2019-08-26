5a14ab2d7e0baf547f911349e627853054a1f03d
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-26 10:36:19 +0200

    add Render method

--- src/View/UserForm.ts
+++ src/View/UserForm.ts
@@ -1,11 +1,18 @@
-export class USerForm {
-    parent: Element;
-    template():string {
-        return '
+export class UserForm {
+	constructor(private parent: Element) {}
+
+	template(): string {
+		return `
             <div>
                 <h1>User Form</h1>
                 <input />
             </div>
-        '
-    }
+        `;
+	}
+
+	render(): void {
+		const templateElement = document.createElement('template');
+		templateElement.innerHTML = this.template();
+		this.parent.append(templateElement.content);
+	}
 }
