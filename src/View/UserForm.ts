5976664122986f294505a51435630568a8fec03f
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-26 10:32:40 +0200

    Add User form class

---
+++ src/View/UserForm.ts
@@ -0,0 +1,11 @@
+export class USerForm {
+    parent: Element;
+    template():string {
+        return '
+            <div>
+                <h1>User Form</h1>
+                <input />
+            </div>
+        '
+    }
+}
